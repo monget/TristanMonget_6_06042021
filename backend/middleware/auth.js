3784fe379cf56df3f9b414a58a239a6480001d14
monget <tristan.monget@laposte.net>
2021-04-29 11:45:20 +0200

    Modification of ID protection

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,26 @@
 const jwt = require('jsonwebtoken');
 
+const User = require('../models/User');
+
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         const userId = decodedToken.userId;
-        if (req.body.userId && req.body.userId !== userId) {
-            throw 'User ID non valable';
-        }
-        else {
-            next();
-        }
+        User.findOne({ _id: userId })
+            .then(user => {
+                if (!user) {
+                    throw 'User ID non valable';
+                }
+                else {
+                    next();
+                }
+            })
+            .catch (error => {
+                res.status(401).json({ error: 'Requête non authentifié !' })
+            })
     }
     catch (error) {
-        res.status(401).json({ error: error | 'Requête non authentifié !' })
+        res.status(401).json({ error: 'Requête non authentifié !' })
     }
 }
